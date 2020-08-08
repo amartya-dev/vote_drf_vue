40be58504e50f7f4b7cc536ca322591d105b7794
amartya-dev <amarkaushik1999@gmail.com>
2020-08-08 13:05:51 +0530

    completed drf backend

---
+++ main/models.py
@@ -0,0 +1,41 @@
+from django.db import models, IntegrityError
+
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=250)
+    details = models.TextField()
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Vote(models.Model):
+    ip_address = models.CharField(
+        max_length=50,
+        default="None",
+        unique=True
+    )
+    candidate = models.ForeignKey(
+        to=Candidate,
+        on_delete=models.CASCADE
+    )
+
+    def save(self, commit=True, *args, **kwargs):
+
+        if commit:
+            try:
+                self.candidate.votes += 1
+                self.candidate.save()
+                super(Vote, self).save(*args, **kwargs)
+
+            except IntegrityError:
+                self.candidate.votes -= 1
+                self.candidate.save()
+                raise IntegrityError
+
+        else:
+            raise IntegrityError
+
+    def __str__(self):
+        return self.candidate.name
